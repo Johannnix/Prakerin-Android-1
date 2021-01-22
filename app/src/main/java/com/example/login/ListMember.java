22a8e6ae614a91e4e75a83e05e8d89bac005365c
Johan Winarto <johanpbrc@gmail.com>
2021-01-22 13:57:47 +0700

    Menghapus Custom Dialog

--- app/src/main/java/com/example/login/ListMember.java
+++ app/src/main/java/com/example/login/ListMember.java
@@ -18,101 +18,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListMember extends AppCompatActivity {
-
-    private TextView txtName;
-    private Button btnOpenDialog;
-
-    private Dialog customDialog;
-    private EditText txtInputName;
-    private Button btnInsertName;
+    RecyclerView listView;
+    Repository repository;
+    MyAdapter adapter;
+    List<User> list;
+    Button input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_member);
 
-        initViews();
-    }
+        listView = findViewById(R.id.listUser);
+        input = findViewById(R.id.inputButton);
 
-    private void initViews(){
-        initCustomDialog();
-        initViewComponents();
-    }
+        repository = new Repository();
 
-    private void initCustomDialog(){
-        customDialog = new Dialog(ListMember.this);
-        customDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        customDialog.setContentView(R.layout.custom_dialog);
-        customDialog.setCancelable(true);
+        list = new ArrayList<>();
 
-        txtInputName = customDialog.findViewById(R.id.txtInputName);
-        btnInsertName = customDialog.findViewById(R.id.btnInsertName);
-        btnInsertName.setOnClickListener(new View.OnClickListener() {
+        input.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String name = txtInputName.getText().toString();
-                txtName.setText(name);
-                customDialog.dismiss();
+            public void onClick(View view) {
+                Intent intent = new Intent(ListMember.this, InputActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    private void initViewComponents(){
-        txtName = findViewById(R.id.txtName);
-        btnOpenDialog = findViewById(R.id.btnOpenDialog);
-        btnOpenDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                customDialog.show();
-            }
-        });
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //clear previous data in list
+        list.clear();
+
+        //get data user from database
+        list.addAll(repository.getAllData());
+
+        adapter = new MyAdapter(list, this);
+
+        listView.setLayoutManager(new LinearLayoutManager(this));
+        listView.setHasFixedSize(true);
+        listView.setItemAnimator(new DefaultItemAnimator());
+        listView.setAdapter(adapter);
+
+        //renew list
+        adapter.notifyDataSetChanged();
     }
 }
-
-//    RecyclerView listView;
-//    Repository repository;
-//    MyAdapter adapter;
-//    List<User> list;
-//    Button input;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_list_member);
-//
-//        listView = findViewById(R.id.listUser);
-//        input = findViewById(R.id.inputButton);
-//
-//        repository = new Repository();
-//
-//        list = new ArrayList<>();
-//
-//        input.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(ListMember.this, InputActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//    }
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        //clear previous data in list
-//        list.clear();
-//
-//        //get data user from database
-//        list.addAll(repository.getAllData());
-//
-//        adapter = new MyAdapter(list, this);
-//
-//        listView.setLayoutManager(new LinearLayoutManager(this));
-//        listView.setHasFixedSize(true);
-//        listView.setItemAnimator(new DefaultItemAnimator());
-//        listView.setAdapter(adapter);
-//
-//        //renew list
-//        adapter.notifyDataSetChanged();
-//
-//
-//    }
