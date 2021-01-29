57906429ae6ffab2c1b2017830ad1a76c5329b52
Johan Winarto <johanpbrc@gmail.com>
2021-01-29 14:42:29 +0700

    Final CRUD Member

--- app/src/main/java/com/example/login/MyAdapter.java
+++ app/src/main/java/com/example/login/MyAdapter.java
@@ -1,12 +1,14 @@
 package com.example.login;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -34,6 +36,47 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
         MyViewHolder item=(MyViewHolder) viewHolder;
         User user=list.get(i);
+
+        Button btnEdit = item.itemView.findViewById(R.id.btnEdit);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View mView = LayoutInflater.from(context).inflate(R.layout.activity_lihat_biodata,null);
+
+                AlertDialog alertDialog = new AlertDialog.Builder(context)
+                        .setView(mView)
+                        .show();
+
+                DaoSession daoSession;
+                daoSession = ((MyApp) ((Activity) context).getApplication()).getDaoSession();
+
+                Button btnEdit2 = alertDialog.findViewById(R.id.button_rubah);
+
+                btnEdit2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        UserDao userDao = daoSession.getUserDao();
+                        User user1 = new User();
+                        EditText nama = mView.findViewById(R.id.name);
+                        EditText alamat = mView.findViewById(R.id.address);
+                        EditText kelamin = mView.findViewById(R.id.kelamin);
+                        EditText telp = mView.findViewById(R.id.telepon);
+                        Button btnSimpan = mView.findViewById(R.id.button_rubah);
+
+                        user1.setId(user.getId());
+                        user1.setName(nama.getText().toString());
+                        user1.setAddress(alamat.getText().toString());
+                        user1.setKelamin(kelamin.getText().toString());
+                        user1.setTelepon(telp.getText().toString());
+                        userDao.update(user1);
+
+                        Toast.makeText(context,"Berhasil Di Update",Toast.LENGTH_SHORT).show();
+                        alertDialog.dismiss();
+                    }
+                });
+
+            }
+        });
 
         Button btnHapus = item.itemView.findViewById(R.id.btnHapus);
         btnHapus.setOnClickListener(new View.OnClickListener() {
